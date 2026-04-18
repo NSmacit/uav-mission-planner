22d051a68c00fbe7a54b23b291bfd5ea050617f1
NSmacit <enes.macit3429@gmail.com>
2026-04-18 18:17:27 +0100

    Add OpenSky authentication and aircraft tracking improvements

--- server.js
+++ server.js
@@ -8,8 +8,15 @@ app.use(express.static('.'));
 
 app.get('/api/aircraft', async (req, res) => {
     try {
-        const response = await fetch('https://opensky-network.org/api/states/all?lamin=45&lomin=-15&lamax=63&lomax=15')
-        const data = await response.json();
+        const username = 'nsmacit29';
+        const password = 'Kelkitli293429.';
+        const credentials = Buffer.from(`${nsmacit29}:${Kelkitli293429.Authorization}`).toString('base64');
+        
+        const response = await fetch('https://opensky-network.org/api/states/all?lamin=45&lomin=-15&lamax=61&lomax=15', {
+            headers: {
+                'Authorization': `Basic ${credentials}`
+            }
+        });        const data = await response.json();
         res.json(data);
     } catch (err) {
         res.json({ states: [] });
